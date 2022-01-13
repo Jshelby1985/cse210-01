ccd78fcbd112ae9336ce6c66e94aa2434fa22390
Jshelby1985 <48341895+Jshelby1985@users.noreply.github.com>
2022-01-12 22:30:49 -0600

    split tic tac toe game

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,11 +1,40 @@
-import buildBoard as Board
 
+from newBoard import create_board
+from winner import has_winner
+from draw import is_a_draw
 
 def main():
-    winner = "N"
-    while winner == "N":    
-        def gameboard(Board):
-            print(gameboard)
+    player = next_player("")
+    board = create_board()
+    while not (has_winner(board) or is_a_draw(board)):
+        display_board(board)
+        make_move(player, board)
+        player = next_player(player)
+    display_board(board)
+    if is_a_draw(board):
+        print("Good game. Thanks for playing!") 
+    if has_winner(board):
+        print("Winner!!")
 
+def display_board(board):
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
+    
 
-main()
+def make_move(player, board):
+    square = int(input(f"{player}'s turn to choose a square (1-9): "))
+    board[square - 1] = player
+
+def next_player(current):
+    if current == "" or current == "o":
+        return "x"
+    elif current == "x":
+        return "o"
+
+if __name__ == "__main__":
+    main()
